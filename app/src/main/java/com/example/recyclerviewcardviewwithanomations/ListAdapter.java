791f53284a4a9fa32997285884ba6444ee929d9d
Javi Romero <javirg1@gmail.com>
2020-11-15 19:25:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewcardviewwithanomations/ListAdapter.java
@@ -0,0 +1,73 @@
+package com.example.recyclerviewcardviewwithanomations;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+    private List<ListElement> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+
+    public ListAdapter(List<ListElement> itemList, Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.mData = itemList;
+    }
+
+    @Override
+    public int getItemCount() { return mData.size(); }
+
+
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.from(parent.getContext()).inflate(R.layout.list_element, parent,false);
+        return new ListAdapter.ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position) {
+        holder.cv.setAnimation(AnimationUtils.loadAnimation(context,R.anim.fade_transition));
+        holder.bindData(mData.get(position));
+    }
+
+
+    public void setItems(List<ListElement> items) { mData = items; }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iconImage;
+        TextView name, city, status;
+        CardView cv;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            iconImage = itemView.findViewById(R.id.iconImageView);
+            name = itemView.findViewById(R.id.nameTextView);
+            city = itemView.findViewById(R.id.cityTextView);
+            status = itemView.findViewById(R.id.statusTextView);
+            cv = itemView.findViewById(R.id.cv);
+        }
+
+
+        void bindData(final ListElement item) {
+            iconImage.setColorFilter(Color.parseColor(item.getColor()), PorterDuff.Mode.SRC_IN);
+            name.setText(item.getName());
+            city.setText(item.getCity());
+            status.setText(item.getStatus());
+        }
+    }
+}
