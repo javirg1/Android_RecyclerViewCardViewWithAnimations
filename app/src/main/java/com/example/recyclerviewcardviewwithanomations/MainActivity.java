ba892363e8b4ed42e5bb0239f0de21512d6ec9f5
Javi Romero <javirg1@gmail.com>
2020-11-15 20:22:33 +0100

    Se añade onClick del CardView. Se cambian colores

--- app/src/main/java/com/example/recyclerviewcardviewwithanomations/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewcardviewwithanomations/MainActivity.java
@@ -24,19 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void init() {
         elements = new ArrayList<>();
-        elements.add(new ListElement("#775447", "Pedro", "México", "Activo"));
-        elements.add(new ListElement("#607d8b", "Julio", "Tabasco", "Activo"));
-        elements.add(new ListElement("#03a9f4", "Alejandra", "Chihuahua", "Cancelado"));
-        elements.add(new ListElement("#f44336", "Jessica", "Durango", "Inactivo"));
-        elements.add(new ListElement("#252255", "Carlos", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
-        elements.add(new ListElement("#009688", "Armando", "Yucatan", "Activo"));
+        elements.add(new ListElement("#775447", "Ana", "Abadiño", "Activo"));
+        elements.add(new ListElement("#607d8b", "Laura", "Durango", "Activo"));
+        elements.add(new ListElement("#03a9f4", "Javi", "Ermua", "Cancelado"));
+        elements.add(new ListElement("#f44336", "Ainhoa", "Zaldibar", "Inactivo"));
+        elements.add(new ListElement("#252255", "Paz", "Durango", "Activo"));
+        elements.add(new ListElement("#015254", "Paz", "Durango", "Activo"));
+        elements.add(new ListElement("#605845", "Bego", "Abadiño", "Activo"));
+        elements.add(new ListElement("#052548", "Haritz", "Ermua", "Activo"));
+        elements.add(new ListElement("#264584", "Javi", "Durango", "Activo"));
+        elements.add(new ListElement("#010504", "Asier", "Iurreta", "Activo"));
+        elements.add(new ListElement("#895424", "Ander", "Berriz", "Activo"));
+        elements.add(new ListElement("#254a54", "Juan Carlos", "Elorrio", "Activo"));
+        elements.add(new ListElement("#5564b5", "Edu", "Ermua", "Activo"));
+        elements.add(new ListElement("#624582", "Sandra", "Durango", "Activo"));
+        elements.add(new ListElement("#655425", "Marina", "Amorebieta", "Activo"));
+
 
         ListAdapter listAdapter = new ListAdapter(elements, this);
         RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
